c921c7c3c7ad1eb362159a55820a52bb6c4748d0
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-08 20:02:49 +0000

    Printing with puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @str: string or array of characters to be printed
+ *
+ * Description: Prints a string using the puts() function
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return (0);
+}
