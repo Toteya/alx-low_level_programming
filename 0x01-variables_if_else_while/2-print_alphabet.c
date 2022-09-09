c906e6cbe7d87e88d5bc6c10e667b68a9a60bbf4
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-09 11:43:11 +0000

    Print entire alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print the alphabet in lowercase
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
