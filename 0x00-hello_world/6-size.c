41fa1a78a5798e9a2ec25891827c1f5897a252c3
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-10 08:38:24 +0000

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long ll;
 	float f;
 
-	printf("Size of a char: %d\n", sizeof(c));
-	printf("Size of a int: %d\n", sizeof(i));
-	printf("Size of a long int: %d\n", sizeof(l));
-	printf("Size of a long long int: %d\n", sizeof(ll));
-	printf("Size of a float: %d\n", sizeof(f))
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of a int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(ll));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
