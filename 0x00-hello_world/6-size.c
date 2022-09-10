4fb376831060afa6e09813f9344a7c230595c5aa
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-10 08:25:13 +0000

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the sizes of various data types on this computer
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long l;
+	long long ll;
+	float f;
+
+	printf("Size of a char: %d\n", sizeof(c));
+	printf("Size of a int: %d\n", sizeof(i));
+	printf("Size of a long int: %d\n", sizeof(l));
+	printf("Size of a long long int: %d\n", sizeof(ll));
+	printf("Size of a float: %d\n", sizeof(f))
+	return (0);
+}
