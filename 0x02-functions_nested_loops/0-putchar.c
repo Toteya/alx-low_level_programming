be96f64fc2cb489824e7d424ae1739eba9968204
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-13 11:04:40 +0000

    Task1: Print _putchar with _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * main - Entry point
+ *
+ * Description
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN
+#define MAIN
+
+/* _putchar - writes character c to standard output */
+int _putchar(char c);
+#endif
