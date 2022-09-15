8c6dd41fac880fcb5b9da69363408ec3c40061ed
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-15 20:37:19 +0000

    Task7: Print a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Prints diagonal lines on the screen
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	return (0);
+}
