f2af495fef56bfdec15170fcad5c43e8427936d2
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-15 08:42:29 +0000

    Task3: Multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplies two number and returns their product.
+ * @a: The first operand to be multiplied.
+ * @b: The second operand to multipied.
+ *
+ * Return: The product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
