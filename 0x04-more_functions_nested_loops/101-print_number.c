7119f952ea2c3a27423965ba69e14db6f7956ca2
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-16 21:30:12 +0000

    Task12: Print any number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - Prints any given number n using the _putchar() function.
+ * @n: The integer to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int i = 5;
+	int j;
+	int ch;
+	int e10, e10_1;
+	int blank = 1;
+
+	if (n < 0)		/* print negative numbers */
+	{
+		n = -n;
+		_putchar('-');
+	}
+
+	for (i = 9; i > 0; i--)
+	{
+		e10 = 1;
+		for (j = 0; j < i; j++)
+		{
+			e10 *= 10;
+		}
+		e10_1 = e10 / 10;
+
+		ch = ((n % e10) - (n % e10) % e10_1) / e10_1;
+		if (blank == 1)		/* Allows insigficant zeros not be printed */
+		{
+			if (ch != 0)
+				blank = 0;
+		}
+
+		if (ch == 0 && blank == 1 && i != 1)	/* skip insignificant zeros */
+			continue;
+		else
+			_putchar(ch + '0');
+	}
+}
