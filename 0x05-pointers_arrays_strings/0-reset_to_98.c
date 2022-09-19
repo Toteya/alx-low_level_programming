bee576ea24875a9564f153994a12d3d51c75df1c
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-19 14:03:11 +0000

    Task0: Update parameter using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - updates parameter pointed to by pointer n to 98.
+ * @n: pointer to the parameter to be updated
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
