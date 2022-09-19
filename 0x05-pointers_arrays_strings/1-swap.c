78d42d3900818fe125784f0812c0a4a39aa70b2a
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-19 14:18:31 +0000

    Task1: Swap values of two variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - Swaps the values of two integer variable
+ * @a: The parameter whose value is to be swapped with the second paramter
+ * @b: The parameter whose value is to be swapped with the first paramter
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+	
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
