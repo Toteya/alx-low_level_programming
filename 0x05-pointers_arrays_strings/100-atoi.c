f5b00f66c1022fe968f97ee900e4bae725a9ebd9
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-20 12:29:22 +0000

    Task10: Covert string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: Pointer to the string that is to be converted
+ *
+ * Return: the integer from the string
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int j;
+	int n_flag = 0;
+	int n_start = 0, n_end = 0, n_len = 0;
+	int num = 0;
+	int pow10 = 1;
+	int count_neg = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (n_flag == 0 && (*(s + i) >= '0' && *(s + i) <= '9'))
+		/* check if current character in the string is a numerical character */
+		{
+			n_flag = 1;
+			n_start = i;
+		}
+		else if (n_flag == 1 && !(*(s + i) >= '0' && *(s + i) <= '9'))
+		/* check if numerical sub-string has ended */
+		{
+			break;
+		}
+		if (*(s + i) == '-')
+			count_neg++;
+		i++;
+		n_end = i;
+	}
+	n_len = n_end - n_start;
+	for (i = n_start; i < n_end; i++)
+	{
+		for (j = 1; j < (n_len - (i - n_start)); j++)
+		{
+			pow10 *= 10;
+		}
+		num += (*(s + i) - '0') * pow10;
+		pow10 = 1;
+	}
+	if (count_neg % 2 != 0)
+		num *= -1;
+	return (num);
+}
+
+
+
+
+
+
+
+
+
+
+
