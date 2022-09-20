0fdd4146b77cc4b31dad79643679fa38782fb996
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-20 13:26:48 +0000

    Task10: Convert string to integer, betty fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,10 +7,9 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int j;
-	int n_flag = 0;
-	int n_start = 0, n_end = 0, n_len = 0;
 	int num = 0;
+	int j;
+	int n_flag = 0, n_start = 0, n_end = 0, n_len = 0;
 	int pow10;
 	int sign = -1;
 	int count_neg = 0;
@@ -25,13 +24,11 @@ int _atoi(char *s)
 		}
 		else if (n_flag == 1 && !(*(s + i) >= '0' && *(s + i) <= '9'))
 		/* check if numerical sub-string has ended */
-		{
 			break;
-		}
 		if (*(s + i) == '-')
 			count_neg++;
 		i++;
-		if(n_flag == 1)
+		if (n_flag == 1)
 			n_end = i;
 	}
 	n_len = n_end - n_start;
@@ -43,9 +40,7 @@ int _atoi(char *s)
 	{
 		pow10 = sign;
 		for (j = 1; j < (n_len - (i - n_start)); j++)
-		{
 			pow10 *= 10;
-		}
 		num += (*(s + i) - '0') * pow10;
 		pow10 = 1;
 	}
