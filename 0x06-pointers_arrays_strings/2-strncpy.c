07c08ea4d3e89c99df891a16d917475e5e1abec8
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-21 18:00:05 +0000

    Task 2: Copy part of a string, correction 1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,13 +9,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
 	{
 		*(dest + i) = *(src + i);
-		if (*(src + i) == '\0')
-		{
-			break;
-		}
 	}
+	for ( ; i < n; i++)
+	{
+		*(dest + i) = '\0';
+	}
+
 	return (dest);
 }
