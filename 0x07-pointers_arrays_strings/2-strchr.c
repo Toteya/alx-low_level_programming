bf0ca851785575393a82598fe9c1fef9320e8699
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-26 10:57:15 +0000

    Task2: Locate a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+/**
+ * _strchr - Locates a given character in a string
+ * @s: Pointer to a string in which to find the character
+ * @c: The character to be located a the string
+ *
+ * Return: Pointer to the memory address containing the first occurence of c.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *ptr;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			ptr = &s[i];
+			break;
+		}
+	}
+	return (ptr);
+}
