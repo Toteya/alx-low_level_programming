c381f70f9987ab768bb95074432d3babbfc9de1e
Toteya <111172685+Toteya@users.noreply.github.com>
2022-09-26 17:36:21 +0000

    Task5: Search for substring within a string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+/**
+ * _strstr - Locates the first occurence of a given substring in a string
+ * @haystack: Pointer to the string in which to search for the substring
+ * @needle: Pointer to a substring to for which to search in haystack
+ *
+ * Return: Pointer to the address of the beginning of the located substring,
+ * otherwise, if no matches are found, return 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j = 0;
+	int found = 0;
+	char *f = 0;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[j])
+		{
+			if (!found)
+				f = &needle[j];
+			j++;
+			found = 1;
+		}
+		else if (found)
+		{
+			j = 0;
+			found = 0;
+		}
+		if (needle[j] == '\0' && found)
+		{
+			break;
+		}
+	}
+	if (!found)
+		*f = 0;
+	return (f);
+}
