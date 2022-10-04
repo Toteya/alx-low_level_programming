be13e2b7cfc9afb87eecbd11efa9734024b88f1e
Toteya <111172685+Toteya@users.noreply.github.com>
2022-10-04 19:48:13 +0000

    Task1: Print number of arguments passed to program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints number of arguments passed to the program
+ * @argc: The number of arguments passed to the program
+ * @argv: Pointer to an array of pointers to string arguments
+ * supplied to the program
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
