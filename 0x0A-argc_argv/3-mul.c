a439e0a26f07ba5bdf21487afd56857aca5cf95e
Toteya <111172685+Toteya@users.noreply.github.com>
2022-10-04 20:33:49 +0000

    Task3: Multiply two numbers passed as arguments to the program, Fix 1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,8 +12,15 @@ int main(int argc, char *argv[])
 {
 	int mul;
 
-	(void) argc;
-	mul = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", mul);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
 	return (0);
 }
