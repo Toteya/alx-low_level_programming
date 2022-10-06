f891a377f99ff816e3a840bdbd40b1d0553bb104
Toteya <111172685+Toteya@users.noreply.github.com>
2022-10-06 13:57:59 +0000

    Task0: Malloc - create an array, null pointer check

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 
 /**
- * create_array - Creates an array of chars and initializes with a specific char
+ * create_array - Creates an array of chars and initializes with a
+ * specific char
  * @size: Size of the array
  * @c: Character with which to initilize the array
  *
@@ -12,12 +13,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ptr = (char *) malloc(sizeof(char) * size);
 
-	if (size == 0)
+	if (size == 0 || ptr == NULL)
 		return (NULL);
-	else
-	{
-		for (i = 0; i < size; i++)
-			ptr[i] = c;
-	}
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
 	return (ptr);
 }
