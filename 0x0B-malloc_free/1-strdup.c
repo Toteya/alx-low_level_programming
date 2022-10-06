cc39273a2714bba78d8ce9b8f74342812454f383
Toteya <111172685+Toteya@users.noreply.github.com>
2022-10-06 16:55:21 +0000

    Task1: Copy a string - malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strdup - allocate space in memory containing a copy of a
+ * string given as a parameter
+ * @str: Pointer to a string to be copied
+ *
+ * Return: Pointer to string,
+ * otherwise if str is NULL, return NULL.
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i, size = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		size++;
+	}
+	s = (char *) malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+		s[i] = str[i];
+	return (s);
+}
