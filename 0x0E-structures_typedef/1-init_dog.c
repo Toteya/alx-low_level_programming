c918cd1d5d510ea8c9b06f75911d473ce6248aff
Toteya <111172685+Toteya@users.noreply.github.com>
2022-10-17 17:36:35 +0000

    Task1: Initialise a struct variable

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to variable type dog
+ * @name: string to be initialised to member - name
+ * @age: float to be initialised to member - age
+ * @owner: string to be initialised to member - owner
+ * Return: Nothing.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
