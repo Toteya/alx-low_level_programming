e31288b370cb3a67e21af87fc677764e3336e97d
Toteya <111172685+Toteya@users.noreply.github.com>
2022-10-31 20:25:37 +0000

    Task1: Concatenate two strings, bug fix 2 - passed NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,10 +12,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, size_1 = 0, size_2 = 0;
 	char *ptr;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		size_1++;
-	for (i = 0; s2[i] != '\0' && i < n; i++)
-		size_2++;
+	if (s1 != NULL)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			size_1++;
+	}
+	if (s2 != NULL)
+	{
+		for (i = 0; s2[i] != '\0' && i < n; i++)
+			size_2++;
+	}
 	ptr = malloc(sizeof(char) * (size_1 + size_2 + 1));
 	if (ptr == NULL)
 		return (NULL);
