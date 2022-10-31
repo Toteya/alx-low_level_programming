ea56f09e71cc07cc639a35aa76837af2efc06201
Toteya <111172685+Toteya@users.noreply.github.com>
2022-10-31 19:02:14 +0000

    Task0: Allocate memory with malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: Number of bytes of memory to allocate.
+ *
+ * Return: Pointer to the allocated memory address
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
