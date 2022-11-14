d5d6a807139d3a71d8d0380c50c1c89f07abc4a8
Toteya <111172685+Toteya@users.noreply.github.com>
2022-11-14 15:17:51 +0000

    Task2: Prints indefinite number of strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Prints strings
+ * @separator: String to separate arguments when printed
+ * @n: Number of arguments
+ * 
+ * Return: Nothing.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+	char *s;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(ap, char *);
+		printf("%s", s);
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
