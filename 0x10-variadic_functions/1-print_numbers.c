487d13fca518c36d3da2fd98b983b31beef5f64d
Toteya <111172685+Toteya@users.noreply.github.com>
2022-11-14 06:25:27 +0000

    Task1: Print indefinite number of arguments, Betty fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,8 +2,8 @@
 
 /**
  * print_numbers - Prints all the numbers passed as arguments
- * @separator:
- * @n:
+ * @separator: String used as separator between arguments
+ * @n: Number of arguments to be printed
  *
  * Return: Nothing.
  */
@@ -12,11 +12,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	va_start (ap, n);
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if(i < n - 1 && separator != NULL)
+		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 	va_end(ap);
