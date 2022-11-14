fdd24a2ac30b03b16908f25e8512fb70f201ed06
Toteya <111172685+Toteya@users.noreply.github.com>
2022-11-14 06:15:35 +0000

    Task1: Print indefinite number of arguments

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints all the numbers passed as arguments
+ * @separator:
+ * @n:
+ *
+ * Return: Nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start (ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if(i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
