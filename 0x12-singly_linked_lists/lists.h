bbe1c82c813e50c5d782c22e97858a103176b961
Toteya <111172685+Toteya@users.noreply.github.com>
2022-11-15 13:03:28 +0000

    Task0: Linked Lists, Betty fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: pointer to next node
+ *
+ * Description: singly-linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;
