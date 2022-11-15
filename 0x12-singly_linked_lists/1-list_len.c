9572caf6304f46fc75a5bab0aaba42c1da4fefb6
Toteya <111172685+Toteya@users.noreply.github.com>
2022-11-15 13:37:04 +0000

    Task1: Number of elements in linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - Return the number of elements in a linked list
+ * @h: Pointer to first node of the list
+ *
+ * Return: size_t - Number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		if (h->next == NULL)
+			break;
+		h++;
+	}
+	return (n);
+}
