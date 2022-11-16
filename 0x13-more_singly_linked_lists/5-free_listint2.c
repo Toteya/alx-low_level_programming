469c3711b1cb5ed7e840de2485ebbaef64777c03
Toteya <111172685+Toteya@users.noreply.github.com>
2022-11-16 19:43:23 +0000

    Task5: Free a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list
+ * @head: Double pointer to the first node of the list
+ *
+ * Return: Nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *old;
+
+	if (*head == NULL)
+	{
+		do {
+			old = *head;
+			*head = (*head)->next;
+			free(old);
+		} while ((*head != NULL));
+	}
+}
