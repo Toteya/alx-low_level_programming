c523ef8184ec7401e4b5c51d1c6b5debe3e4e51b
Toteya <111172685+Toteya@users.noreply.github.com>
2022-11-22 14:37:51 +0000

    Task3: Set bit at given index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+/**
+ * set_bit - Sets the value of a bit to 1 at the given index.
+ * @n: The number whose bit's value is to be set.
+ * @index: The index of the bit to be set.
+ *
+ * Return: If successful, 1. Otherwise, return -1.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+	unsigned long int b = 1;
+
+	if (index > 64) /* index is out of range long unsigned int */
+		return (-1);
+	for (i = 0; i < index; i++)
+	{
+		b = b << 1;
+	}
+	*n = *n | b;
+	return (-1);
+}
+
