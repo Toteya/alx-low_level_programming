8ed79741bbdb92f5275b5d969aab7de601120aae
Toteya <111172685+Toteya@users.noreply.github.com>
2022-11-22 13:14:05 +0000

    Task2: Get a bit from a number

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,10 +14,10 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	for (i = 0; i < index; i++)
 	{
-		if (n <= 1)
+/*		if (n <= 1)
 		{
 			return (-1);
-		}
+		} */
 		n = n >> 1;
 	}
 	if ((n % 2) == 0)
